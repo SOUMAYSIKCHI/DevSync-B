bcded2becf85b9ca8bc426afb789a586ca312dad
SOUMAYSIKCHI <120584256+SOUMAYSIKCHI@users.noreply.github.com>
2025-06-11 10:06:13 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,21 @@
-// 📦 Required Modules
 const express = require("express");
 const userRoute = express.Router(); // Initialize router instance for user-specific endpoints
-// 🧠 Controller Imports
+const Job = require("../models/jobModel");
 const { authorization } = require("../controllers/authorization"); // Middleware to secure user-specific routes
 const { requestReceived, connections, feed } = require("../controllers/userConnection"); // Controller functions
 
 
 userRoute.get("/user/request/received", authorization, requestReceived);
-
 userRoute.get("/user/connections", authorization, connections);
-
 userRoute.get("/user/feed",authorization,feed);
+userRoute.get("/user/jobs",authorization,async(req,res)=>{
+    try{
+        const jobs = await Job.find().sort({ postedDate: -1 }); 
+        res.status(200).json(jobs);
+    }catch(err){
+        console.error('Error fetching jobs:', error.message);
+        res.status(500).json({ error: 'Server error while fetching jobs' });
+    }
+});
 
 module.exports = userRoute;
